17e2593ace9ff7c97a6eddc6e4c3cd473c68c38f
iAngelGuzman <agelgzman@outlook.com>
2025-05-05 12:58:42 -0600

    08-fieldset

--- html00/js/pagina07.js
+++ html00/js/pagina07.js
@@ -3,7 +3,14 @@ const boton = document.getElementById("enviar");
 boton.addEventListener("click", function() {
     console.log("Formulario enviado");
     let user = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let correo = document.getElementById("email");
     let pass = document.getElementById("password");
+    let confirmarpass = document.getElementById("password2");
     console.log(user.value);
+    console.log(apellido.value);
+    console.log(correo.value);
     console.log(pass.value);
+    console.log(confirmarpass.value);
+    console.log("Contraseña igual: " + pass.value == confirmarpass.value)
 });
